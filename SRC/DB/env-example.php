5f40748963f6fbc32eef249cd7785a67cd43aab7
ocorvu <professorjoaodamata@gmail.com>
2023-05-06 12:18:58 -0300

    adicionado exemplo de conexão SQLITE.

--- SRC/DB/env-example.php
+++ SRC/DB/env-example.php
@@ -1,11 +1,22 @@
 <?php
 
-//
-$_ENV['HOST'] = 'localhost';
-$_ENV['DRIVER'] = 'mysql';
-$_ENV['DATABASE'] = 'estoque';
-$_ENV['USER'] = 'root';
-$_ENV['PORT'] ='5432';
+// MYSQL CONNECTION
+
+// $_ENV['HOST'] = 'localhost';
+// $_ENV['DRIVER'] = 'mysql';
+// $_ENV['DATABASE'] = 'estoque';
+// $_ENV['USER'] = 'root';
+// $_ENV['PORT'] ='3306';
+// $_ENV['PASSWORD'] = '';
+// $_ENV['URI'] ='';
+
+// SQLITE CONNECTION
+
+$_ENV['HOST'] = '';
+$_ENV['DRIVER'] = 'sqlite';
+$_ENV['DATABASE'] = 'db.sqlite3';
+$_ENV['DATABASE_PATH'] = 'SRC/DB/Migrations/db.sqlite3';
+$_ENV['USER'] = '';
+$_ENV['PORT'] ='';
 $_ENV['PASSWORD'] = '';
 $_ENV['URI'] ='';
-
