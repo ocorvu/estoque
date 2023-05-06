21ca51adba2f157785b60dd29f67c247401af991
ocorvu <professorjoaodamata@gmail.com>
2023-05-06 12:22:30 -0300

    adicionado nova conexão para suporte ao SQLITE

--- SRC/DB/Database.php
+++ SRC/DB/Database.php
@@ -11,17 +11,31 @@ class Database
     public static function conection()
     {
         try {
-            $dsn = $_ENV['DRIVER'] . ':host=' . $_ENV['HOST'] . ';port=' . $_ENV['PORT'] . ';dbname=' . $_ENV['DATABASE'];
-
-            $conn = new PDO($dsn, $_ENV['USER'], $_ENV['PASSWORD'], [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            ]);
+            if ($_ENV['DRIVER'] == 'sqlite') {
+                if (basename(getcwd()) == 'estoque') {
+                    $dsn = $_ENV['DRIVER'] . ':' . $_ENV['DATABASE_PATH'];
+                }
+                else {
+                    $dsn = $_ENV['DRIVER'] . ':' . $_ENV['DATABASE'];
+                }
+                $conn = new PDO($dsn, options: [
+                    \PDO::ATTR_EMULATE_PREPARES => false,
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+                ]);
+            }
+            else {
+                $dsn = $_ENV['DRIVER'] . ':host=' . $_ENV['HOST'] . ';port=' . $_ENV['PORT'] . ';dbname=' . $_ENV['DATABASE'];
+                $conn = new PDO($dsn, $_ENV['USER'], $_ENV['PASSWORD'], [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ]);
+            }
         
             if ($conn) {
                 return $conn;
             }
         } catch (PDOException $e) {
-            die($e->getMessage());
+            die(PHP_EOL . $e->getMessage() . PHP_EOL . PHP_EOL);
         }
     }
 
