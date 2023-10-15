85be4b99cdacd5e679ee59a3dd478177f4703238
ocorvu <profdamata@gmail.com>
2023-10-15 12:56:04 -0300

    atualização do ano e target do linkedin

--- SRC/VIEW/partials/footer.php
+++ SRC/VIEW/partials/footer.php
@@ -1,10 +1,10 @@
 <footer class="pb-10">
-    <p class="center" id="copy">Copyright 2021 &copy - João da Mata</p>
+    <p class="center" id="copy">Copyright 2023 &copy - João da Mata</p>
     <p class="flex">
         <a class="link" href="https://github.com/ocorvu" target="_blank">
             <img class="mr-5" src="<?= asset('IMAGES/github.png') ?>" alt="icone github">Github
         </a>
-        <a class="link" href="https://www.linkedin.com/in/joaofhdm/" target="_blanl">
+        <a class="link" href="https://www.linkedin.com/in/joaofhdm/" target="_blank">
             <img class="mr-5" src="<?= asset('IMAGES/linkedin.png') ?>" alt="icone linkedin">Linkedin
         </a>  
         <a class="link" href="https://twitter.com/damatajao" target="_blank">
